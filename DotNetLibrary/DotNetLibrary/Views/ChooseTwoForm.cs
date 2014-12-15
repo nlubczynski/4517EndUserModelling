ce6ea207ab6c436f54b1b5cdc1dced45da71f82b
Braden Lunn <braden.lunn@gmail.com>
2014-12-14 19:11:40 -0500

    the form for my project file

---
+++ DotNetLibrary/DotNetLibrary/Views/ChooseTwoForm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DotNetLibrary.Views
+{
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    public partial class ChooseTwoForm : Form
+    {
+        public Options SelectedValue
+        {
+            get;
+            private set;
+        }
+
+        public enum Options
+        {
+            OPTION_ONE = 0,
+            OPTION_TWO,
+        }
+
+        public ChooseTwoForm()
+        {
+            InitializeComponent();
+        }
+
+        public void setParams(string promptText, string titleText, string optionOneText, string optionTwoText)
+        {
+            lblPrompt.Text = promptText;
+            this.Text = titleText;
+            btnFirstOption.Text = optionOneText;
+            btnSecondOption.Text = optionTwoText;
+        }
+
+        private void btnFirstOption_Click(object sender, EventArgs e)
+        {
+            this.SelectedValue = Options.OPTION_ONE;
+            this.Close();
+        }
+
+        private void btnSecondOption_Click(object sender, EventArgs e)
+        {
+            this.SelectedValue = Options.OPTION_TWO;
+            this.Close();
+        }
+    }
+}
